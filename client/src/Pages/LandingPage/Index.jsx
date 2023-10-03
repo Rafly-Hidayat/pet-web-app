d5e64db33128e08f042967993415d1e65d05bfb3
Rafly-Hidayat <rafly.hidayat2710@gmail.com>
2023-10-03 21:58:19 +0700

    update landing page

--- client/src/Pages/LandingPage/Index.jsx
+++ client/src/Pages/LandingPage/Index.jsx
@@ -1,59 +1,24 @@
-import landingPage1 from "../../Assets/Image/landingPage1.png";
-import quickAndEasy from "../../Assets/Image/quickAndEasy.png";
-import qualityService from "../../Assets/Image/qualityService.png";
-import fromHome from "../../Assets/Image/fromHome.png";
-import technology from "../../Assets/Image/technology.png";
-import { Wave } from "../../Assets/svg/SVG";
-import petStep from "../../Assets/svg/petStep.svg";
+import landingPage1 from "../../Assets/Image/LandingPage.png";
 import logo from "../../Assets/Image/logo.png";
-import { useNavigate } from "react-router-dom";
-
+import { BsWhatsapp, BsTiktok } from "react-icons/bs";
+import { FaMapMarkedAlt } from "react-icons/fa";
 export default function Index() {
-  const navigate = useNavigate();
-
-  // Function to scroll to the section with the given ID
-  const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
   return (
-    <div className="h-screen w-screen overflow-auto">
-      <div className="w-full h-20 shadow-sm flex justify-between items-center px-5 sm:px-10 inset-0  absolute top-0 backdrop-blur-md bg-white/30">
-        <div className="flex items-center space-x-4">
+    <div className="bg-[#F6FFFD] w-screen h-screen relative overflow-x-hidden">
+      <div className="w-full h-20 flex items-center px-5 sm:px-10 inset-0 absolute top-0">
+        <div className="flex items-center space-x-3">
           <img src={logo} alt="" className="w-7 sm:w-10 aspect-square" />
           <div className="text-xl sm:text-2xl font-semibold">Petwebcare</div>
         </div>
-        <div className="hidden items-center space-x-2 sm:flex">
-          <div
-            className="cursor-pointer"
-            onClick={() => scrollToSection("misi")}
-          >
-            Misi Kami
-          </div>
-          <div className="w-2 h-2 rounded-full bg-[#f3ab7b]"></div>
-          <div
-            className="cursor-pointer"
-            onClick={() => scrollToSection("layanan")}
-          >
-            Layanan Kami
-          </div>
-          <div> | </div>
-          <div className="cursor-pointer" onClick={() => navigate("/login")}>
-            Daftar
-          </div>
-        </div>
       </div>
-      <div className="scroll-smooth overflow-scroll w-full h-full">
-        {/* Slogan */}
-        <div className="bg-[#ffffff] w-full sm:grid grid-cols-2 gap-4  content-center pt-24 pb-4">
+      <section className="w-full h-full relative">
+        <div className="flex items-center h-full w-full justify-between">
           <div
             id="1"
-            className="flex flex-col items-center md:items-start md:pl-10 justify-center h-full space-y-2"
+            className="md:w-[50%] h-full flex flex-col items-center md:items-start md:pl-10 justify-center space-y-2"
           >
             <div
-              className="w-[80%] h-[250px] rounded-t-full bg-cover bg-center sm:hidden"
+              className="w-[80%] h-[250px] bg-cover bg-center sm:hidden"
               style={{ backgroundImage: `url(${landingPage1})` }}
             />
             <div
@@ -65,178 +30,33 @@ export default function Index() {
             <div className="pl-2 text-center md:text-start text-2xl italic text-slate-700">
               Solusi Keluhan Hewan Peliharan Anda
             </div>
+            <div className="pl-2">
+              <div className="border border-[#FF834F] p-3 rounded-full cursor-pointer text-[#FF834F] hover:bg-[#FF834F] hover:text-white">
+                Daftar Sekarang &gt;&gt;
+              </div>
+            </div>
           </div>
-          <div
-            className="h-[450px] rounded-l-full bg-cover bg-center hidden sm:flex"
-            style={{ backgroundImage: `url(${landingPage1})` }}
+          <img
+            src={landingPage1}
+            alt="landing page"
+            className="w-[650px] hidden md:flex"
           />
         </div>
-        {/* Misi */}
-        <div
-          id="misi"
-          className="bg-[#F8F5F2] w-full h-full flex flex-col px-10"
-        >
-          <div
-            className="w-full text-center my-5 text-4xl text-[#92a5a5]"
-            style={{ fontFamily: "Lobster" }}
-          >
-            Misi Utama Kami
+        <div className="absolute bottom-10 left-10 flex items-center space-x-7">
+          <div className="space-x-1 flex items-center">
+            <BsWhatsapp className="text-xl" />
+            <div>909090909090</div>
           </div>
-          <div className="flex items-start space-x-4 lg:space-x-1 overflow-x-auto   no-scrollbar">
-            <div
-              id="1"
-              className="flex flex-col min-w-[80%] lg:min-w-[40%] space-y-4 items-center justify-center "
-            >
-              <img
-                src={quickAndEasy}
-                alt="Mudah dan Cepat"
-                className="w-80 aspect-square rounded-3xl border-slate-500 border-4"
-              />
-              <div className="text-lg text-slate-700 text-center">
-                Mudah dan Cepat
-              </div>
-              <div className="md:max-w-[80%] text-slate-700 text-center lg:text-justify">
-                Menyediakan akses mudah dan cepat ke perawatan kesehatan hewan
-                peliharaan yang berkualitas, di mana pun dan kapan pun. Kami
-                bertekad untuk meningkatkan kesejahteraan hewan peliharaan
-                dengan memberikan akses ke dokter hewan terpercaya dan
-                berpengalaman
-              </div>
-            </div>
-            <div
-              id="2"
-              className="flex flex-col min-w-[80%] lg:min-w-[40%] space-y-4 items-center justify-center "
-            >
-              <img
-                src={qualityService}
-                alt="Mudah dan Cepat"
-                className="w-80 aspect-square rounded-3xl border-slate-500 border-4"
-              />
-              <div className="text-lg text-slate-700 text-center">
-                Pelayanan Berkualitas
-              </div>
-              <div className="md:max-w-[80%] text-slate-700 text-center lg:text-justify">
-                Kami berkomitmen untuk menyediakan pelayanan konsultasi dokter
-                hewan yang berkualitas tinggi. Dokter hewan kami adalah para
-                profesional berlisensi yang peduli dengan kesehatan hewan
-                peliharaan Anda
-              </div>
-            </div>
-            <div
-              id="3"
-              className="flex flex-col min-w-[80%] lg:min-w-[40%] space-y-4 items-center justify-center "
-            >
-              <img
-                src={fromHome}
-                alt="Mudah dan Cepat"
-                className="w-80 aspect-square rounded-3xl border-slate-500 border-4"
-              />
-              <div className="text-lg text-slate-700 text-center">
-                Kenyamanan Pemilik Hewan Peliharaan
-              </div>
-              <div className="md:max-w-[80%] text-slate-700 text-center lg:text-justify">
-                Kami berusaha untuk membuat pengalaman konsultasi secepat dan
-                sesederhana mungkin. Dengan platform kami, Anda dapat
-                menghubungi dokter hewan dari kenyamanan rumah Anda sendiri,
-                mengurangi stres bagi hewan peliharaan Anda
-              </div>
-            </div>
-            <div
-              id="4"
-              className="flex flex-col min-w-[80%] lg:min-w-[40%] space-y-4 items-center justify-center "
-            >
-              <img
-                src={technology}
-                alt="Mudah dan Cepat"
-                className="w-80 aspect-square rounded-3xl border-slate-500 border-4"
-              />
-              <div className="text-lg text-slate-700 text-center">
-                Inovasi dalam Layanan Kesehatan Hewan
-              </div>
-              <div className="md:max-w-[80%] text-center lg:text-justify">
-                Kami selalu mencari cara untuk meningkatkan layanan kami melalui
-                inovasi teknologi dan metode perawatan terbaru. Misi kami adalah
-                tetap menjadi pemimpin dalam industri kesehatan hewan
-              </div>
-            </div>
+          <div className="space-x-1 flex items-center">
+            <BsTiktok className="text-xl" />
+            <div>abcdefg</div>
+          </div>
+          <div className="space-x-1 flex items-center">
+            <FaMapMarkedAlt className="text-xl" />
+            <div>Jln 123</div>
           </div>
         </div>
-        {/* Services */}
-        <div className="w-full bg-[#ffffff] -mt-3">
-          <Wave fill="#F8F5F2" />
-          <div
-            id="layanan"
-            className="my-5 w-full md:w-[80%] md:h-[550px] bg-[#E8ECED] md:rounded-r-full md:flex flex-col justify-center items-center md:space-y-20"
-          >
-            <div
-              className="py-5 md:py-0 md:w-[80%] text-center text-3xl text-[#92a5a5]"
-              style={{ fontFamily: "Lobster" }}
-            >
-              Layanan Kami
-            </div>
-            <div className="px-10 flex items-center space-x-10">
-              <div className="space-y-5">
-                <div className="flex items-center justify-center space-x-2">
-                  <img
-                    src={petStep}
-                    alt="pet step"
-                    className="hidden md:flex w-5 aspect-square"
-                  />
-                  <div className="text-slate-700 text-lg">
-                    Realtime Chat dengan Dokter Hewan
-                  </div>
-                  <img
-                    src={petStep}
-                    alt="pet step"
-                    className="hidden md:flex w-5 aspect-square"
-                  />
-                </div>
-                <div className="text-slate-700">
-                  Dapatkan Akses Langsung ke Perawatan Hewan Peliharaan Anda!
-                  Dengan Realtime Chat, Anda dapat menghubungi dokter hewan
-                  terpercaya kami kapan saja, di mana pun Anda berada. Tidak
-                  perlu menunggu lagi untuk menjawab pertanyaan Anda atau
-                  memberikan bantuan dalam situasi darurat. Kesehatan hewan
-                  peliharaan Anda dalam genggaman Anda. Chat langsung dengan
-                  dokter hewan sekarang!
-                </div>
-              </div>
-              {/* <div className="space-y-5">
-                  <div className="flex items-center justify-center space-x-2">
-                    <img
-                      src={petStep}
-                      alt="pet step"
-                      className="w-5 aspect-square"
-                    />
-                    <div className="text-slate-700 text-lg">
-                      Jadwalkan Kunjungan Langsung
-                    </div>
-                    <img
-                      src={petStep}
-                      alt="pet step"
-                      className="w-5 aspect-square"
-                    />
-                  </div>
-                  <div className="text-slate-700">
-                    Perawatan yang Personal dan Menyeluruh untuk Hewan Peliharaan
-                    Anda! Jadwalkan Kunjungan Langsung dengan dokter hewan kami
-                    untuk pemeriksaan yang lebih mendalam dan perawatan yang
-                    disesuaikan. Kami hadir di tempat Anda, memberikan perawatan
-                    yang diberikan dengan cinta dan perhatian. Jangan kompromi
-                    dalam merawat hewan peliharaan Anda, jadwalkan kunjungan
-                    sekarang untuk kesehatan yang optimal!
-                  </div>
-                </div> */}
-            </div>
-          </div>
-        </div>
-        {/* Footer */}
-        <div className="w-full">
-          <div className="text-center md:py-4 text-slate-700">
-            @ Petwebcare 2023
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
