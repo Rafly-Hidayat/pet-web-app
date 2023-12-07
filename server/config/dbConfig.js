5a099eae78bfd53e5a1cd11f6cbf430c6f83aaad
Rafly-Hidayat <rafly.hidayat2710@gmail.com>
2023-12-07 20:49:57 +0700

    update db config

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,7 +1,14 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DATABASE: 'bulubulu',
+    HOST: 'bha1odavyw93mh3vevs7-mysql.services.clever-cloud.com',
+    USER: 'urjfnseyl0tk0sjk',
+    PASSWORD: 'FJ7fYRqLxDZmnVu64RFY',
+    DATABASE: 'bha1odavyw93mh3vevs7',
     DIALECT: 'mysql',
 }
+// module.exports = {
+//     HOST: 'localhost',
+//     USER: 'root',
+//     PASSWORD: '',
+//     DATABASE: 'bulubulu',
+//     DIALECT: 'mysql',
+// }
